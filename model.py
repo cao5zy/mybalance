de7824060a4f7b65805d8c5f983d403d35f15742
Zongying Cao <zongying.cao@hpe.com>
2017-12-26 20:41:36 +0800

    complete get budget remaining

--- model.py
+++ model.py
@@ -47,12 +47,18 @@ class Budget:
 
 		def getBalanceAccountRemaining():
 			# 获取账期内，扣除账户预算后的余额
-			print(getAccountConsumption())
-			print(getAccountBudget())
+			def getBudgetRemaining(accBudget, accCon):
+				getRemaining = lambda key: accBudget[key] - accCon[key] if accBudget[key] - accCon[key] > 0 else 0
+				return accBudget.update(dict(map(lambda key:(key, getRemaining(key)), \
+				filter(lambda key:key in accCon, [key for key in accCon])))) or accBudget
 
-			def getBudgetRemaining():
-				pass
-			pass
+			def getSumOfBudgetRemaining(budgetRemaining):
+				return sum(map(lambda key:budgetRemaining[key], budgetRemaining))
+
+			return getSumOfIncome() \
+				- getSumOfConsumption() \
+				- getSumOfBudgetRemaining(getBudgetRemaining(getAccountBudget(), getAccountConsumption()))
+
 
 		self.addConsumption = addConsumption
 		self.addBudget = addBudget
