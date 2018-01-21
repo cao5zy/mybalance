5cc4a6c8da907ccd7625eadc55947e5e6c507766
Zongying Cao <zongying.cao@hpe.com>
2018-01-21 14:47:45 +0800

    add schema

--- budget_management.py
+++ budget_management.py
@@ -1,6 +1,40 @@
 import os
 import itertools
 from pymongo import MongoClient
+from datetime import date
+
+# schema defintion
+from mongorm import Field, Database
+db = Database(uri='mongodb://localhost:27017/testdb')
+
+class MyBalance(db.Document):
+	__collection__ = "MyBalances"
+	__fields__ = {
+		"incomes": [{
+			"income": Field.required(float),
+			"date": Field.required(date),
+			"desc": Field.optional(str),
+			"title": Field.required(str)
+		}],
+		"budgets": [{
+			"account": Field.required(str),
+			"amount": Field.required(float)
+		}],
+		"consumptions": [{
+			"consumption": Field.required(str),
+			"account": Field.required(str),
+			"date": Field.required(date),
+			"desc": Field.optional(str)
+		}],
+		"key": Field.required(int)
+	}
+
+class Key(db.Document):
+	__collection__ = "Keys"
+	__fields__ = {
+		"name": Field.required(str),
+		"order": Field.required(int)
+	}
 
 
 class BudgetManagement:
@@ -12,10 +46,26 @@ class BudgetManagement:
 
 	def __init__(self):
 		self.conn = MongoClient("mongodb://localhost", 27017)
-		self.db = self.conn.testdb
+		db = self.conn.testdb
+
+		def currentKey(default="default"):
+			def findKey(col):
+				print(col)
+				print(col.count())
+				return 0 if not col else col.find({name: default}).value
+			return findKey(db["keys"])
+
+		def currentkey():
+			pass
+
+		def currentBalance():
+			pass
+
+		def currentBalance():
+			pass
 
 		def newBalance():
-			self.db["Balances"].insert_many([{"number": 1, "data": {"bds":[{"name": "KFC"}]}}])
+			db["Balances"].insert_many([{"number": 1, "data": {"bds":[{"name": "KFC"}]}}])
 
 			# generate a new balance with name number-date.json
 			pass
@@ -45,3 +95,4 @@ class BudgetManagement:
 		self.newBalance = newBalance
 		self.currentBalance = currentBalance
 		self.historyBalances = historyBalances
+		self.currentKey = currentKey
