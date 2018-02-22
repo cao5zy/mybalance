bbfc214d0d397e13810804fb8e19012c7fac8615
Zongying Cao <zongying.cao@hpe.com>
2018-02-22 20:54:52 +0800

    sum budgets

--- tests/test_present.py
+++ tests/test_present.py
@@ -16,3 +16,24 @@ def test_genBudgetList():
 	assert_that(result).is_length(1)
 	assert_that(result[0]["amount"]).is_equal_to(70)
 	assert_that(result[0]["account"]).is_equal_to("play")
+
+def test_genBudgetListMulti():
+	from present import genBudgetList
+
+	lst = [{
+	"account": "play"
+	,"amount": 30
+	}
+	,{
+	"account": "play"
+	,"amount": 40
+	}
+	,{
+	"account": "other"
+	,"amount": 50
+	}]
+
+	result = genBudgetList(lst)
+	assert_that(result).is_length(2)
+	assert_that(result).contains({"account": "play", "amount": 70})
+	assert_that(result).contains({"account": "other", "amount": 50})
