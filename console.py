7da1c1f8e03b2f2db01116b6bf67308f3c736df0
Zongying Cao <zongying.cao@hpe.com>
2018-02-25 15:55:00 +0800

    refine code

--- console.py
+++ console.py
@@ -1,71 +1,72 @@
 import sys
+import datetime
 from budget_management import BudgetManagement
 
-def getBudgetParam():
+def get_budget_param():
 	return {
 		"account": sys.argv[2]
 		,"amount": sys.argv[3]
 	}
 
-def getConsumptionParam():
+def get_consumption_param():
 	return {
 		"account": sys.argv[2]
 		,"consumption": sys.argv[3]
 		,"desc": "" if len(sys.argv) == 4 else sys.argv[4]
 	}
 
-def getIncomeParam():
+def get_income_param():
 	return {
 		"title": sys.argv[2]
 		,"income": sys.argv[3]
 		,"desc": "" if len(sys.argv) == 4 else sys.argv[4]
 	}
 
-def getAction():
+def get_action():
 	return sys.argv[2]
 
-def addBudget():
-	def add(budgetManagement):
-		budgetManagement.addBudget(budgetManagement.currentBalance(), getBudgetParam())
+def add_budget():
+	def add(budget_management):
+		budget_management.add_budget(budget_management.currentBalance(), get_budget_param())
 
 	add(BudgetManagement())
 
-def addConsumption():
-	def add(budgetManagement):
-		budgetManagement.addConsumption(budgetManagement.currentBalance(), \
-			getConsumptionParam().update({"date": datetime.datetime.now()}))
+def add_consumption():
+	def add(budget_management):
+		budget_management.add_consumption(budget_management.currentBalance(), \
+			get_consumption_param().update({"date": datetime.datetime.now()}))
 
 	add(BudgetManagement())
 
-def addIncome():
-	def add(budgetManagement):
-		budgetManagement.addIncome(budgetManagement.currentBalance(), \
-			getIncomeParam().update({"date": datetime.datetime.now()}))
+def add_income():
+	def add(budget_management):
+		budget_management.add_income(budget_management.currentBalance(), \
+			get_income_param().update({"date": datetime.datetime.now()}))
 
 	add(BudgetManagement())
 
-def errorCmd():
+def error_cmd():
 	print("error command")
 
-def showBudgetRemaining():
-	from present import showBudgetRemaining
-	def show(budgetManagement):
-		showBudgetRemaining(budgetManagement.toJson(budgetManagement.currentBalance()))
+def show_budget_remaining():
+	import present
+	def show(budget_management):
+		present.showBudgetRemaining(budget_management.toJson(budget_management.currentBalance()))
 
 	show(BudgetManagement())
 
 def main():
 	def process(action):
-		(lambda actionDic:actionDic[(action if action in actionDic else "errorCmd").lower()]())\
+		(lambda actionDic:actionDic[(action if action in actionDic else "error_cmd").lower()]())\
 		({
-			"addbudget": addBudget
-			,"addconsumption": addConsumption
-			,"addincome": addIncome
-			,"errorcmd": errorCmd
-			,"showbudgetremaining": showBudgetRemaining
+			"addbudget": add_budget
+			,"addconsumption": add_consumption
+			,"addincome": add_income
+			,"errorcmd": error_cmd
+			,"showbudgetremaining": show_budget_remaining
 		})
 
-	process(getAction())
+	process(get_action())
 
 if __name__ == '__main__':
 	main()
